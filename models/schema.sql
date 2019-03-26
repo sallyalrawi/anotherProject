644c7b7dc0c6792f59e92fad56b8a1e0c913612e
sally <sallyakrum83@gmail.com>
2019-03-25 20:28:49 -0500

    added the data-base values

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,20 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+DROP DATABASE IF EXISTS hookahzon_db;
+CREATE DATABASE hookahzon_db;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+USE hookahzon_db;
+CREATE TABLE products(
+ item_id INT(10) NOT NULL auto_increment,
+ product_name VARCHAR(100) NOT NULL,
+ department VARCHAR(45) NOT NULL,
+ price INT default 0,
+ quantity INT default 0,
+  PRIMARY KEY (item_id)
+);
+INSERT INTO products (product_name,department,price, quantity)
+VALUES ("Electronic Hookah", "Hookah Tool", 100,80);
+
+INSERT INTO products (product_name,department,price, quantity)
+VALUES ("Charcoals Hookah", "Hookah Tool", 120,30);
+
+
+
